3603db2ebbf3611dadcb94d797a102e9307ea632
alexander <vertiporogov.alexandr@mail.ru>
2023-12-23 14:29:24 +0300

    finish

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dogs.urls')),
+    path('', include('dogs.urls', namespace='dogs')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
